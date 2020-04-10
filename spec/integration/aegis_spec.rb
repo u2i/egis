3fa5f9615aace15efaa3bbe216b6b1f95356ce98
Iwona Grabczak <iwona.grabczak@u2i.com>
2020-04-10 16:43:44 +0200

    updated integration test

--- spec/integration/aegis_spec.rb
+++ spec/integration/aegis_spec.rb
@@ -21,15 +21,15 @@ RSpec.describe 'Integration with AWS Athena' do
     database = client.database("aegis_integration_test_#{test_id}")
     database.drop
     database.create!
+    database.exists? || raise('Database does not exist!!')
 
     table = database.table('test_table', schema, "s3://#{testing_bucket}/test_input_data/#{test_id}")
     table.create!
     table.create
 
-    table.add_partitions!(country: %w[us mx], language: [1, 2])
-    table.add_partitions(country: %w[us mx], language: [1, 2])
-
-    result = database.execute_query('SELECT * FROM test_table ORDER BY id;', async: false)
+    table.upload_data(input)
+    database.execute_query("INSERT INTO test_table VALUES (5, 'and hello again', 'it', 3)", async: false)
+    result = table.download_data
 
     database.drop!
     database.drop
@@ -41,38 +41,27 @@ RSpec.describe 'Integration with AWS Athena' do
 
   let(:testing_bucket) { 'aegis-integration-testing' }
 
+  let(:input) do
+    [
+      [1, 'hello world', 'mx', 1],
+      [2, 'hello again', 'mx', 2],
+      [3, 'hello once more', 'us', 1],
+      [4, 'hello for the fourth time', 'us', 2]
+    ]
+  end
   let(:expected_output) do
-    <<~CSV
-      "id","message","country","language"
-      "1","hello world","mx","1"
-      "2","hello again","mx","2"
-      "3","hello once more","us","1"
-      "4","hello for the fourth time","us","2"
-    CSV
+    [
+      [1, 'hello world', 'mx', 1],
+      [2, 'hello again', 'mx', 2],
+      [3, 'hello once more', 'us', 1],
+      [4, 'hello for the fourth time', 'us', 2],
+      [5, 'and hello again', 'it', 3]
+    ]
   end
 
   let(:s3) { Aws::S3::Client.new }
 
-  before do
-    puts "Test ID: #{test_id}"
-    puts 'Uploading testing data to S3'
-    dir = File.expand_path(File.join(File.dirname(__FILE__), 'test_input_data'))
-
-    Dir.glob(File.join('**', '*'), base: dir).reject { |path| File.directory?(File.join(dir, path)) }.each do |path|
-      source_file_path = File.join(dir, path)
-      s3_path = "test_input_data/#{test_id}/#{path}"
-
-      puts "#{source_file_path} -> #{s3_path}"
-
-      s3.put_object(bucket: testing_bucket, key: s3_path, body: File.read(source_file_path))
-    end
-  end
-
   it 'goes though the whole query execution flow successfully', integration: true do
-    result = subject
-
-    output_csv = s3.get_object(bucket: result.output_location.bucket, key: result.output_location.key).body.read
-
-    expect(output_csv).to eq(expected_output)
+    expect(subject).to match_array(expected_output)
   end
 end
