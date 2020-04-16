075ae98492987e7014422d0bb3f6b443886c5dc8
Marek Mateja <marek.mateja@u2i.com>
2020-04-16 10:56:35 +0200

    Move cleanup logic to TestingMode class and run cleanup only when testing featues have been used

--- lib/aegis/testing.rb
+++ lib/aegis/testing.rb
@@ -14,17 +14,6 @@ module Aegis
     yield
   ensure
     @mode = previous_mode
-    Testing.remove_databases(test_id)
-  end
-
-  module Testing
-    def self.remove_databases(test_id)
-      client = Aegis::Client.new
-      output_downloader = Aegis::OutputDownloader.new
-
-      result = client.execute_query("SHOW DATABASES LIKE '#{test_id}.*';", async: false)
-      query_result = output_downloader.download(result.output_location)
-      query_result.flatten.each { |database| client.database(database).drop }
-    end
+    test_mode.cleanup
   end
 end
